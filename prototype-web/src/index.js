3f55dd3ab57c27d9e35ed69c536850995fb81c29
Philip Ekblom <philip.ekblom@gmail.com>
2018-01-30 22:00:05 +0100

    Menu added

--- prototype-web/src/index.js
+++ prototype-web/src/index.js
@@ -3,28 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class TestElement extends React.Component {
-  render() {
-    const text = 'Hello';
-
-    return (
-        <div className="text">{text}</div>
-    );
-  }
+    render() {
+        return (
+            <div id="menu">
+                <div className="menu_button">
+                    <i className="fab fa-cloudsmith"/>
+                    <div className="menu_text">Overview</div>
+                </div>
+                <div className="menu_button">
+                    <i className="fas fa-chart-line"/>
+                    <div className="menu_text">Statistics</div>
+                </div>
+                <div className="menu_button">
+                    <i className="far fa-calendar"/>
+                    <div className="menu_text">Planning</div>
+                </div>
+                <div className="menu_button">
+                    <i className="fas fa-location-arrow"/>
+                    <div className="menu_text">Map</div>
+                </div>
+                <div className="menu_button">
+                    <i className="fas fa-cog"/>
+                    <div className="menu_text">Settings</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 class Page extends React.Component {
-  render() {
-    return (
-      <div className="container">
-		<TestElement/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container">
+                <TestElement/>
+            </div>
+        );
+    }
 }
 
 // ========================================
 
 ReactDOM.render(
-  <Page/>,
-  document.getElementById('root')
+    <Page/>,
+    document.getElementById('root')
 );
