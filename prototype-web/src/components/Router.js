6c572a4c5262637d35e992095d7b14a9e79da920
Philip Ekblom <philip.ekblom@gmail.com>
2018-01-31 19:35:12 +0100

    Conflict solved

--- prototype-web/src/components/Router.js
+++ prototype-web/src/components/Router.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Overview from './overview/Overview';
-<<<<<<< HEAD
 import Map from './map/Map';
-=======
 import Stats from './stats/Stats';
->>>>>>> 1b96607cf29c7c4b81ea1b72e8be550206601782
 
 const Router = () => (
     <main>
         <Switch>
             <Route exact path='/' component={Overview}/>
-<<<<<<< HEAD
             <Route exact path='/map' component={Map}/>
-=======
 			<Route exact path='/Planning' component={Stats}/>
->>>>>>> 1b96607cf29c7c4b81ea1b72e8be550206601782
         </Switch>
     </main>
 );
