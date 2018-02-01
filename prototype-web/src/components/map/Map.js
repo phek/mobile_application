5459e081139807fade37ca053886a125f6088458
Philip Ekblom <philip.ekblom@gmail.com>
2018-02-01 02:23:12 +0100

    Added non-https geolocation

--- prototype-web/src/components/map/Map.js
+++ prototype-web/src/components/map/Map.js
@@ -1,28 +1,61 @@
 import React from 'react';
 import {Map, GoogleApiWrapper} from 'google-maps-react';
+import $ from 'jquery';
 import './map.css';
 import TitleBar from '../building-blocks/titlebar/TitleBar';
 
+
 class MapContainer extends React.Component {
 
-    placeMarker(mapProps, map) {
+    constructor(props) {
+        super(props);
+
+        /* Hi my name is React and I want to bind my function before I use them */
+        this.tryGeolocation = this.tryGeolocation.bind(this);
+        this.tryGoogleGeolocation = this.tryGoogleGeolocation.bind(this);
+        this.placeMarker = this.placeMarker.bind(this);
+    }
+
+    tryGoogleGeolocation(callback) {
+        $.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBQnLL3XdhKRxWiqpF_tex24_2T370tzio", function (success) {
+            const pos = {
+                lat: success.location.lat,
+                lng: success.location.lng
+            };
+            callback(pos);
+        }).fail(function (err) {
+            alert("Geolocation error!");
+            console.log(err);
+        });
+    }
+
+    tryGeolocation(callback) {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                const {google} = mapProps;
-                const pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-                new google.maps.Marker({
-                    position: pos,
-                    map: map,
-                    title: 'Hello World!'
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+                    callback(pos);
+                },
+                (error) => {
+                    this.tryGoogleGeolocation(callback);
                 });
-                map.setCenter(pos);
-            });
-        } else {
-            //"Geolocation is not supported by this browser."
         }
+    };
+
+
+    placeMarker(mapProps, map) {
+        this.tryGeolocation(function (position) {
+            const {google} = mapProps;
+            new google.maps.Marker({
+                position: position,
+                map: map,
+                title: 'Hello World!'
+            });
+            map.setCenter(position);
+        });
     }
 
     render() {
