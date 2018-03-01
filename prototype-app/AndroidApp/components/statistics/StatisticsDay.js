e39f62ffc41eb4cf1007a9ef79751440c0d51233
Tobbe <tudd@kth.se>
2018-03-01 01:22:23 +0100

    dynamically rendered activities from api

--- prototype-app/AndroidApp/components/statistics/StatisticsDay.js
+++ prototype-app/AndroidApp/components/statistics/StatisticsDay.js
@@ -14,7 +14,7 @@ export default class StatisticsDay extends React.Component {
     }
 
     getCategoriesFromApi(){
-        return fetch('http://84.217.10.60:3000/productivity/categories')
+        return fetch('http://84.217.10.60:3000/productivity/processLog')
             .then((response) => response.json())
             .then((responseJson) => {
                 this.setState({data: responseJson});
@@ -25,6 +25,17 @@ export default class StatisticsDay extends React.Component {
     }
 
     render() {
+
+        let activityArr = this.state.data;
+        let activityList = activityArr.map( activity => <View style={{flexDirection: 'row'}} padding={10}>
+            <View style={[styles.activity, {flex: 1, height: 100}]}><Text
+                style={styles.text}>{Math.round((activity.stop - activity.start)/1000)}</Text></View>
+            <View style={{width: 1, backgroundColor: '#afafaf'}}/>
+            <View style={[styles.activity, {flex: 2, height: 100}]}><Text
+                style={styles.text}>{activity.name}</Text></View>
+        </View>
+        );
+
         return (
             <View style={{
                 flex: 1,
@@ -33,27 +44,7 @@ export default class StatisticsDay extends React.Component {
             }}>
                 <View style={{flex: 4, flexDirection: 'row', backgroundColor: '#dbdbdb'}}>
                     <ScrollView style={{flex: 2, backgroundColor: '#cdcdcd'}}>
-                        <View style={{flexDirection: 'row'}} padding={10}>
-                            <View style={[styles.activity, {flex: 1, height: 100}]}><Text
-                                style={styles.text}>{this.state.data.gaming}</Text></View>
-                            <View style={{width: 1, backgroundColor: '#afafaf'}}/>
-                            <View style={[styles.activity, {flex: 2, height: 100}]}><Text
-                                style={styles.text}>Gaming</Text></View>
-                        </View>
-                        <View style={{flexDirection: 'row'}} padding={10}>
-                            <View style={[styles.activity, {flex: 1, height: 100}]}><Text
-                                style={styles.text}>{this.state.data.productive}</Text></View>
-                            <View style={{width: 1, backgroundColor: '#afafaf'}}/>
-                            <View style={[styles.activity, {flex: 2, height: 100}]}><Text
-                                style={styles.text}>Productive</Text></View>
-                        </View>
-                        <View style={{flexDirection: 'row'}} padding={10}>
-                            <View style={[styles.activity, {flex: 1, height: 100}]}><Text
-                                style={styles.text}>{this.state.data.browsing}</Text></View>
-                            <View style={{width: 1, backgroundColor: '#afafaf'}}/>
-                            <View style={[styles.activity, {flex: 2, height: 100}]}><Text
-                                style={styles.text}>Browsing</Text></View>
-                        </View>
+                        {activityList}
                     </ScrollView>
                 </View>
             </View>
